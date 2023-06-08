f164af79f0275daf1926a93045adecdbc12fbd5b
Mre-Dennis-Kinuthia <denniskinuthia1105@gmail.com>
2023-06-08 10:06:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import LoginForm from './pages/LoginPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <LoginForm />
-      </nav>
-    </div>
+    <Router>
+      <div>
+        <NavigationBar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          {/* Add other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
